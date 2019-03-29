315284c794402fff3525c0b4470dd45fe41bff33
Jianmin <jianminwei1@gmail.com>
2019-03-29 07:57:58 -0400

    added sqllite sqlalchemy configuration to the config.py

--- app/config.py
+++ app/config.py
@@ -1,2 +1,17 @@
+import os
 class Configuration(object):
     DEBUG = True
+
+    # __file__ is the full file name (include path) of the module
+    APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
+
+    # define a sqlalchemy URI
+    # dialect+driver://username:password@host:port/database
+    # Because SQLite databases are stored in local files, the only information we need to
+    # provide is the path to the database file.
+    #
+    # On the other hand, if you wanted to connect
+    # to PostgreSQL running locally, your URI might look something like this:
+    # postgresql://postgres:secretpassword@localhost:5432/blog_db
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///%s/blog.db' % APPLICATION_DIR
