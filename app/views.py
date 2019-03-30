91c36426117062295176845ba29c70a25d2ab319
Jianmin <jianminwei1@gmail.com>
2019-03-30 10:13:52 -0400

    add parameter name to the template

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 
 @app.route('/')
 def homepage():
-    return render_template('homepage.html')
+    name = request.args.get('name')
+    if not name:
+        name = '<unknown>'
+    return render_template('homepage.html', name=name)
