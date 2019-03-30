7ab7fdc4828049626504c7a8eecf6dccad485d35
Jianmin <jianminwei1@gmail.com>
2019-03-29 20:06:20 -0400

    added Tag, Entry_tag to the models.py

--- app/models.py
+++ app/models.py
@@ -5,6 +5,12 @@ from app import db
 def slugify(s):
    return re.sub('[^\w]+', '-', s).lower()
 
+entry_tags = db.Table('entry_tags',
+        db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')),
+        db.Column('entry_id', db.Integer, db.ForeignKey('entry.id'))
+)
+
+
 class Entry(db.Model):
    id = db.Column(db.Integer, primary_key=True)
    title = db.Column(db.String(100))
@@ -12,6 +18,8 @@ class Entry(db.Model):
    body = db.Column(db.Text)
    created_timestamp = db.Column(db.DateTime, default=datetime.datetime.now)
    modified_timestamp = db.Column( db.DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)
+
+   tags = db.relationship('Tag', secondary = entry_tags, backref = db.backref('entries', lazy = 'dynamic'))
 
    def __init__(self, *args, **kwargs):
       super(Entry, self).__init__(*args, **kwargs) # Call parent constructor.
@@ -25,5 +33,22 @@ class Entry(db.Model):
    def __repr__(self):
       return '<Entry: %s>' % self.title
 
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(64))
+    slug = db.Column(db.String(64), unique = True)
+
+    def __init__(self, *args, **kwargs):
+        super(Tag, self).__init__(*args, **kwargs)
+        self.slug = slugify(self.name)
+
+    def __repr__(self):
+        return '<Tag %s>' % self.name
+
+
+
 if __name__ == '__main__':
     db.create_all()
+
+
+
