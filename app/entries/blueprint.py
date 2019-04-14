1ea77835dfb47fa292dc5284db8b2779f7122af8
Jianmin <jianminwei1@gmail.com>
2019-04-14 15:18:21 -0400

    added full text search to entries and tag_detail pages

--- app/entries/blueprint.py
+++ app/entries/blueprint.py
@@ -8,7 +8,7 @@ entries = Blueprint('entries', __name__, template_folder='templates')
 @entries.route('/')
 def index():
    entries = Entry.query.order_by(Entry.created_timestamp.desc())
-   return object_list('entries/index.html', entries)
+   return entry_list('entries/index.html', entries)
 
 @entries.route('/tags/')
 def tag_index():
@@ -19,10 +19,17 @@ def tag_index():
 def tag_detail(slug):
    tag = Tag.query.filter(Tag.slug == slug).first_or_404()
    entries = tag.entries.order_by(Entry.created_timestamp.desc())
-   return object_list('entries/tag_detail.html', entries, tag=tag)
+   return entry_list('entries/tag_detail.html', entries, tag=tag)
 
 from flask import render_template
 @entries.route('/<slug>/')
 def detail(slug):
    entry = Entry.query.filter(Entry.slug == slug).first_or_404()
    return render_template('entries/detail.html', entry=entry)
+
+from flask import request
+def entry_list(template, query, **context):
+   search = request.args.get('q')
+   if search:
+      query = query.filter( (Entry.body.contains(search)) | (Entry.title.contains(search)))
+   return object_list(template, query, **context)
